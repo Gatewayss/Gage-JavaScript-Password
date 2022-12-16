aa217ef323b55582877f1c90693b026c4aeb3857
Gage Norton <nortongage0@gmail.com>
2022-12-16 11:30:29 -0600

    Create function clg demo

--- script.js
+++ script.js
@@ -7,33 +7,42 @@ let passwordLowerCase = confirm("Would you like lower case characters in your pa
 let lowerCasePreference;
 let passwordUpperCase = confirm("Would you like uppercase characters in your password?")
 let passwordNumbers = confirm("Would you like number in your password?")
-
+let options = []
+let newArray = []
+let lowercaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+let uppercaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+let numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 // Write password to the #password input
 function generatePassword() {
-  if (parsePasswordLength < 7 || parsePasswordLength > 129) {
-    console.log("try again")
-  } else {
-    console.log(parsePasswordLength)
-  } 
-
   if (passwordLowerCase === true) {
-    lowerCasePreference = console.log("yes to lowercase")
+    options = options.concat(lowercaseArray)
+    console.log(options)
   } else {
-    lowerCasePreference = console.log("no to lowercase")
+    console.log("no to lowercase")
   }
-
   if (passwordUpperCase === true) {
-    passwordUpperCase = console.log("yes to uppercase")
+    options = options.concat(uppercaseArray)
+    passwordUpperCase = console.log(options)
   } else {
     passwordUpperCase = console.log("no to uppercase")
   }
 
   if (passwordNumbers === true) {
-    passwordNumbers = console.log("yes to numbers")
+    options = options.concat(numberArray)
+    passwordNumbers = console.log(options)
   } else {
     passwordNumbers = console.log("no to numbers")
   }
+  if (parsePasswordLength < 7 || parsePasswordLength > 129) {
+    console.log("try again")
+  } else {
+    for (let i = 0; i < parsePasswordLength; i++) {
+      newArray.push(options[Math.floor(Math.random() * (28 - 0))])
+      console.log(newArray)
+    }
+  }
 }
+
 generatePassword()
 
 
@@ -46,7 +55,7 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); 
+generateBtn.addEventListener("click", writePassword);
 
 /* todo:
 
