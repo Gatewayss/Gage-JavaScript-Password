4d25e0172752d133aa18c2a24287347b5b8708f6
Gage Norton <nortongage0@gmail.com>
2022-12-15 23:30:54 -0600

    Create TODO list

--- script.js
+++ script.js
@@ -12,3 +12,14 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+/* todo:
+
+click generateBtn to generate password
+prompt one is password length 8-128
+prompt to use lowercase 
+prompt to use uppercase
+prompt to use numbers and or special characters
+once all the prompts are done a password should generate 
+it can be displayed as alert or in the text box 
+*/
